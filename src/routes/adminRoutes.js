51d6b04c8198b2e3b8800aef3a27c5c2e15838bf
Sdg877 <sdrakegill@gmail.com>
2025-01-21 11:56:38 +0000

    News route added

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { loginAdmin } from '../controllers/adminController.js'
+import { addNewsUpdate } from "../controllers/adminController";
 
 const router = express.Router();
 
 router.post('/login', loginAdmin);
+router.post("/news", addNewsUpdate);
 
 export default router;

2d3a096a2d669acde8c4c5f4a17ba9be9c8627cb
Sdg877 <sdrakegill@gmail.com>
2025-01-21 12:05:23 +0000

    Route for adding images

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { loginAdmin, addNewsUpdate } from '../controllers/adminController.js'
+import { loginAdmin, addNewsUpdate, uploadImageToS3 } from '../controllers/adminController.js'
 
 const router = express.Router();
 
 router.post('/login', loginAdmin);
 router.post("/news", addNewsUpdate);
+router.post("/images", uploadImageToS3)
 
 export default router;

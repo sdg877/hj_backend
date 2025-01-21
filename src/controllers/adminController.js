2f38f5817c6015ec3d85823797a9622dd00a470c
Sdg877 <sdrakegill@gmail.com>
2025-01-21 11:55:18 +0000

    News function added

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,8 +1,42 @@
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import News from "../models/newsModel";
 
 dotenv.config();
+
+export const addNewsUpdate = async (req, res) => {
+  const { title, comment } = req.body;
+
+  if (!title || !comment) {
+    return res.status(400).json({ message: "Title and comment are required." });
+  }
+
+  try {
+    const newNewsUpdate = {
+      title,
+      comment,
+      timestamp: new Date(),
+    };
+
+    const news = await News.findOne();
+    if (news) {
+      news.newsUpdates.push(newNewsUpdate);
+      await news.save();
+    } else {
+      const newNews = new News({
+        newsUpdates: [newNewsUpdate],
+      });
+      await newNews.save();
+    }
+
+    res
+      .status(201)
+      .json({ message: "News update added successfully", newNewsUpdate });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 export const loginAdmin = (req, res) => {
   const { username, password } = req.body;

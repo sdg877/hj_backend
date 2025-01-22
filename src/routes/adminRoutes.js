9f132be10a278be6a42cee0caaa3f71b9e3c6ef0
Sdg877 <sdrakegill@gmail.com>
2025-01-22 06:31:51 +0000

    Backend routes updated

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { loginAdmin, addNewsUpdate, uploadImageToS3 } from '../controllers/adminController.js'
+import { loginAdmin, addNewsOnly, uploadImageOnly, addNewsWithImage } from '../controllers/adminController.js'
 
 const router = express.Router();
 
 router.post('/login', loginAdmin);
-router.post("/news", addNewsUpdate);
-router.post("/images", uploadImageToS3)
+router.post("/news", addNewsOnly);
+router.post("/images", uploadImageOnly);
+router.post("/add-news-with-image", addNewsWithImage)
+
 
 export default router;

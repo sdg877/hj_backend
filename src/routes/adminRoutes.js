fa3aad210cf477edb4f534d86d31dfb807e33a09
Sdg877 <sdrakegill@gmail.com>
2025-01-24 08:30:09 +0000

    S3 image upload working successfully

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { loginAdmin, addNewsOnly, uploadImageOnly, addNewsWithImage } from '../controllers/adminController.js'
+import multer from 'multer';
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/login', loginAdmin);
 router.post("/news", addNewsOnly);
-router.post("/images", uploadImageOnly);
+router.post("/images", upload.single('image'), uploadImageOnly);
 router.post("/add-news-with-image", addNewsWithImage)
-
 
 export default router;

6a231aee6c872e83191d5137a8af7a130ba550ee
Sdg877 <sdrakegill@gmail.com>
2025-01-25 06:39:03 +0000

    Protection added to admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,13 +1,21 @@
-import express from 'express';
-import { loginAdmin, addNewsOnly, uploadImageOnly, addNewsWithImage } from '../controllers/adminController.js'
-import multer from 'multer';
+import express from "express";
+import { authenticateJWT } from "../../config/auth.js"; // Ensure correct path
+import {
+  loginAdmin,
+  addNewsOnly,
+  uploadImageOnly,
+  addNewsWithImage
+} from "../controllers/adminController.js";
+import multer from "multer";
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/login', loginAdmin);
-router.post("/news", addNewsOnly);
-router.post("/images", upload.single('image'), uploadImageOnly);
-router.post("/add-news-with-image", addNewsWithImage)
+router.post("/login", loginAdmin);
+
+
+router.post("/news", authenticateJWT, addNewsOnly);
+router.post("/images", authenticateJWT, upload.single("image"), uploadImageOnly);
+router.post("/add-news-with-image", authenticateJWT, upload.single("image"), addNewsWithImage);
 
 export default router;

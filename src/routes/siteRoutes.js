1e1e7ed809f0e7d2e28cc7e47519558dfa782f03
Sdg877 <sdrakegill@gmail.com>
2025-01-27 10:37:31 +0000

    Endpoint for news updates

--- src/routes/siteRoutes.js
+++ src/routes/siteRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getImagesByCategory } from "../controllers/siteController.js";
+import { getNews } from "../controllers/siteController.js";
 
 const router = express.Router();
 
 router.get('/gallery/:category', getImagesByCategory);
+router.get('/news', getNews);
 
 export default router;

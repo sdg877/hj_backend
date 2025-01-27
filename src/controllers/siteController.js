e6fd4cde8b79a2851fb230e64f3e92dd33a8e2e0
Sdg877 <sdrakegill@gmail.com>
2025-01-27 10:36:38 +0000

    Function for displaying news

--- src/controllers/siteController.js
+++ src/controllers/siteController.js
@@ -1,7 +1,8 @@
 import AWS from "aws-sdk";
+import News from "../models/newsModel.js";
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Set your AWS credentials
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
@@ -24,5 +25,15 @@ export const getImagesByCategory = async (req, res) => {
   } catch (error) {
     console.error("Error retrieving images:", error);
     res.status(500).json({ message: "Error retrieving images" });
+  }
+};
+
+export const getNews = async (req, res) => {
+  try {
+    const news = await News.find().sort({ createdAt: -1 }); 
+    res.json({ news });
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    res.status(500).json({ message: "Error fetching news" });
   }
 };

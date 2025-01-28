8d045b31d1b820c9a8f013330c422679525e1346
Sdg877 <sdrakegill@gmail.com>
2025-01-28 07:07:28 +0000

    Routes for updating and deleting news and deleting images

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import { authenticateJWT } from "../../config/auth.js"; 
 import {
   loginAdmin,
-  addNewsOnly,
+  addNews,
   uploadImageOnly,
-  addNewsWithImage
+  deleteImage, 
+  updateNews,
+  deleteNews
 } from "../controllers/adminController.js";
 import multer from "multer";
 
@@ -12,9 +14,10 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/login", loginAdmin);
-
-router.post("/news", authenticateJWT, addNewsOnly);
+router.post("/news", authenticateJWT, addNews);
+router.put("/updatenews/:newsId", authenticateJWT, updateNews);
+router.delete("/deletenews/:newsId", authenticateJWT, deleteNews);
 router.post("/images", authenticateJWT, upload.single("image"), uploadImageOnly);
-router.post("/add-news-with-image", authenticateJWT, upload.single("image"), addNewsWithImage);
+router.delete("/deleteimage", authenticateJWT, deleteImage);
 
 export default router;

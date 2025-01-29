ae48fa3a5580b60135720104b51067bfd273db1b
Sdg877 <sdrakegill@gmail.com>
2025-01-29 10:07:37 +0000

    Update to cors policy

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );

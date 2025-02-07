5d868f0a1469b7755b02d0772802efb8733e3bf9
Sdg877 <sdrakegill@gmail.com>
2025-02-07 05:33:54 +0000

    Update to mongoose connection

--- config/db.js
+++ config/db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
